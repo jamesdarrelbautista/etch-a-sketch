8b87af4741ed602834c1bce0a836a0ef3007b624
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-17 20:03:02 +0800

    first draft of making it draw black color

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ let container = document.querySelector('.container');
 let div;
 
 function createGrid(x) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
+    if(x>100) return 'too big';
+    for (let rows = 0; rows < x; rows++) {
+        for (let columns = 0; columns < x; columns++) {
             div = document.createElement('div');
             div.classList.add('grid');
             container.appendChild(div);
         };
     };
+    let grid = document.querySelectorAll('.grid');
+    let dimension = 960/x;
+    grid.forEach(element => {
+        element.style.width = `${dimension}px`;
+        element.style.height = `${dimension}px`;
+        element.addEventListener('mouseover',(e)=>{
+            console.log(e);
+            e.target.style.backgroundColor = 'black';
+        });
+    });
+    console.log(960/x);
 };
 
-createGrid(2);
+createGrid(10);
