aeab5f1c28ea5cbc2bbc018aef74871b20054c5f
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-18 19:53:18 +0800

    can draw color other than black BUT not random colors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let container = document.querySelector('.container');
 let div;
 
-function createGrid(x) {
+function createGrid(x,rgb) {
     if (x > 100) return 'too big';
     for (let rows = 0; rows < x; rows++) {
         for (let columns = 0; columns < x; columns++) {
@@ -15,14 +15,16 @@ function createGrid(x) {
     grid.forEach(element => {
         element.style.width = `${dimension}px`;
         element.style.height = `${dimension}px`;
+        
         element.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
+            if(rgb == false)e.target.style.backgroundColor = 'black';
+            else e.target.style.backgroundColor = 'rgb(12,100,80)';
         });
     });
     console.log(960 / x);
 };
 
-createGrid(16);
+createGrid(16,false);
 
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
@@ -31,8 +33,15 @@ clear.addEventListener('click', () => {
     grid.forEach(element => {
         container.removeChild(element);
     });
-    createGrid(gridValue);
+    createGrid(gridValue,false);
 });
 
 let rgb = document.querySelector('.rgb');
-
+rgb.addEventListener('click', () => {
+    let grid = document.querySelectorAll('.grid');
+    let gridValue = prompt('How many grid do you want?');
+    grid.forEach(element => {
+        container.removeChild(element);
+    });
+    createGrid(gridValue,true);
+});
